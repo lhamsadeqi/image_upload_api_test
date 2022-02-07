435be76aa349de3cab0aa585bd3777f543bc40ea
lhamsadeqi <elhamsadeqi.es@gmail.com>
2022-02-07 12:06:15 +0330

    creating project

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/widget/image_upload.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ImageUpload extends StatelessWidget {
+  const ImageUpload({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+      width: double.infinity,
+      height: 200,
+    );
+  }
+}
